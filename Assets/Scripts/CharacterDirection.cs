98db0e5b006e787676b081ffa85c9659de298441
Thiago Frensch <thiago.frensch@gmail.com>
2017-06-10 11:59:46 -0300

    primeiro commit

---
+++ Assets/Scripts/CharacterDirection.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterDirection : MonoBehaviour {
+
+    public GameObject bridgeLeft;
+    public GameObject bridgeRight;
+    public GameObject[] towersTop;
+    public GameObject[] towersBottom;
+
+    private bool[] towersTopStatus = { true, true, true};
+    private bool[] towersBottomStatus = { true, true, true};
+
+    GameObject CalculateClosestTower(GameObject[] towers, Vector2 position, bool[] towerStatus, bool towerStatusTrue)
+    {
+        int minIndex = 0;
+        float minDist = 1000;
+        for (int i = 0; i < towers.Length; ++i)
+        {
+            float dist = Vector2.Distance(position, towers[i].transform.position);
+            if(dist < minDist && (towerStatus[i] || !towerStatusTrue) )
+            {
+                minDist = dist;
+                minIndex = i;
+            }
+        }
+        return towers[minIndex];
+    }
+
+	// Update is called once per frame
+	public Vector2 CalcDirection (bool bottomTeam, bool fly, Vector2 position) {
+        Vector2 direction = Vector2.zero;
+        if (bottomTeam)
+        {
+            GameObject tower = CalculateClosestTower(towersTop, position, towersTopStatus, fly);
+            if(!fly && position.y < bridgeLeft.transform.position.y - 0.5f)
+            {
+                GameObject bridge = bridgeLeft;
+                if(Vector2.Distance(position, bridgeLeft.transform.position) >
+                   Vector2.Distance(position, bridgeRight.transform.position))
+                {
+                    bridge = bridgeRight;
+                }
+                direction = ((Vector2)bridge.transform.position - position);
+            } else
+            {
+                direction = ((Vector2)tower.transform.position - position);
+            }
+        }
+        return direction;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PlayerController : MonoBehaviour {
+
+    public CharacterDirection charDir;
+
+    private Animator animator;
+    private float last_horizontal = 0;
+    private float last_vertical = 0;
+    private string animation_direction = "Front";
+    // Use this for initialization
+    public static IEnumerable SceneRoots()
+    {
+        var prop = new HierarchyProperty(HierarchyType.GameObjects);
+        var expanded = new int[0];
+        while (prop.Next(expanded))
+        {
+            yield return prop.pptrValue as GameObject;
+        }
+    }
+
+    void Start () {
+        animator = GetComponent<Animator>();
+        //charDir = transform.root.GetComponentInChildren<CharacterDirection>();
+        if (!charDir)
+            throw new System.Exception();
+    }
+
+    void ChangeMoveAnimation(float horizontal, float vertical)
+    {
+        float MARGIN_UP = 0.2f;
+        float MARGIN_DOWN = -MARGIN_UP;
+        if (horizontal == 0 && vertical == 0)
+        {
+            animator.SetTrigger("Idle");
+        } else if (horizontal <= MARGIN_UP && horizontal >= MARGIN_DOWN)
+        {
+            if (vertical > MARGIN_UP)
+            {
+                animation_direction = "Back";
+            }
+            else if (vertical < MARGIN_DOWN)
+            {
+                animation_direction = "Front";
+            }
+        } else if (vertical <= MARGIN_UP && vertical >= MARGIN_DOWN)
+        {
+            if (horizontal > MARGIN_UP)
+            {
+                animation_direction = "Right";
+            }
+            else if (horizontal < MARGIN_DOWN)
+            {
+                animation_direction = "Left";
+            }
+        } else if (vertical > MARGIN_UP)
+        {
+            if (horizontal > MARGIN_UP)
+            {
+                animation_direction = "BackRight";
+            }
+            else if (horizontal < MARGIN_DOWN)
+            {
+                animation_direction = "BackLeft";
+            }
+        } else if (vertical < MARGIN_DOWN)
+        {
+            if (horizontal > MARGIN_UP)
+            {
+                animation_direction = "FrontRight";
+            }
+            else if (horizontal < MARGIN_DOWN)
+            {
+                animation_direction = "FrontLeft";
+            }
+        }
+        if (horizontal != 0 || vertical != 0)
+        {
+            animator.SetTrigger("Walk");
+        }
+        animator.SetTrigger(animation_direction);
+    }
+
+    static int Sign(float number)
+    {
+        return number < 0 ? -1 : (number > 0 ? 1 : 0);
+    }
+    Vector2 CalcMovement()
+    {
+        Vector2 direction = charDir.CalcDirection(true, false, transform.position);
+
+        //Vector2 movement = new Vector2(Sign(direction.x), Sign(direction.y));
+        Vector2 movement = direction.normalized;
+        Debug.Log(direction + " " + movement );
+        return movement;
+    }
+    // Update is called once per frame 
+    void Update()
+    {
+        float scale = 0.01f;
+        float horizontal = 0;
+        float vertical = 0;
+
+        //horizontal = (int)Input.GetAxis("Horizontal");
+        //vertical = (int)Input.GetAxis("Vertical");
+        Vector2 movement = CalcMovement();
+        horizontal = movement.x;
+        vertical = movement.y;
+
+        if (Sign(last_vertical) != Sign(vertical) || Sign(last_horizontal) != Sign(horizontal))
+            ChangeMoveAnimation(horizontal, vertical);
+
+        last_horizontal = horizontal;
+        last_vertical = vertical;
+        transform.Translate(new Vector3(horizontal* scale, vertical* scale, 0));
+    }
+}
